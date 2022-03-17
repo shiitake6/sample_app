2585296713e15f879010455f0b198a820d1ea9bb
shiitake <shiitake.626@gmail.com>
2022-03-17 15:28:20 +0900

    画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'posts/index'
-  get 'home/top'
+  get "login" => "users#login_form"
+  # ルーティングを追加してください
+  
+
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  
   get "/" => "home#top"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "home/index" => "home/index"
+  get "about" => "home#about"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_13_121949) do
+ActiveRecord::Schema.define(version: 2022_03_17_001038) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
 end
