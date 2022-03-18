2366460a44535aebf7f73b74d18b09c11daed409
shiitake <shiitake.626@gmail.com>
2022-03-18 09:35:14 +0900

    posts model 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_001038) do
+ActiveRecord::Schema.define(version: 2022_03_17_063221) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
